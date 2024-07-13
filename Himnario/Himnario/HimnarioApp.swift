4bdc5ba5acc5a738c9b3abe740f97fc1cad258b1
Student 13 <student13@ITB201-COMP13s-iMac.local>
2024-07-13 12:50:29 +0800

    project initialization

---
+++ Himnario/Himnario/HimnarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HimnarioApp.swift
+//  Himnario
+//
+//  Created by Student 13 on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HimnarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
